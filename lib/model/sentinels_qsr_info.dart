25a0c81353a7332909c3eef61e05a0f025ad8b04
kossmmos <184493470+kossmmos@users.noreply.github.com>
2024-11-14 11:08:12 +0200

    feat: class made serializable

--- lib/model/sentinels_qsr_info.dart
+++ lib/model/sentinels_qsr_info.dart
@@ -4,6 +4,21 @@ class SentinelsQsrInfo {
     required this.cost,
     required this.deposit,
   });
+
+  factory SentinelsQsrInfo.fromJson(Map<String, dynamic> json) {
+      return SentinelsQsrInfo(
+        cost: BigInt.parse(json['cost'] as String),
+        deposit: BigInt.parse(json['deposit'] as String),
+      );
+  }
+
   final BigInt cost;
   final BigInt deposit;
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'cost': cost.toString(),
+      'deposit': deposit.toString(),
+    };
+  }
 }
